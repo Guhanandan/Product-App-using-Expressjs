b8c4619c507004ce0c1203727a77c5f8ed632300
Guhanandan <guhanandan6542060@gmail.com>
2024-04-26 08:53:38 +0530

    getAllProducts function is modified

--- controllers/products.js
+++ controllers/products.js
@@ -5,12 +5,6 @@ const getAllProductsStatic = async (req,res) => {
     res.status(200).json({products , num_of_prod : products.length})
 }
 
-const getAllProducts = async (req,res) => {
-    console.log(req.query)
-    const products = await Product.find(req.query) 
-    res.status(200).json(products)
-}
-
 module.exports = {
     getAllProducts,
     getAllProductsStatic
